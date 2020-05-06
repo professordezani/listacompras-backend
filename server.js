bc1d85010bfd10f29536a85504123f38bc13b045
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-05-06 14:09:25 +0000

    Alterei o texto de retorno do GET.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', function(request, response) {
-    response.send('Hello World, Fatec!');
+    response.send('Lista de Compras');
 })
 
 server.listen(process.env.PORT || 3000);
