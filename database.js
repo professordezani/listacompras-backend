d26eface08a23eef41e29560340da1d643dc1541
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-05-06 15:49:03 +0000

    Adicionei o Postgres e os métodos para criar e consultar.

---
+++ database.js
@@ -0,0 +1,50 @@
+// yarn add pg
+
+const Pool = require('pg').Pool;
+
+// 1 - Abre conexão
+// 2 - Executa comando SQL (select - índice, insert...) 4ms
+// 3 - Fecha a conexão
+
+const pool = new Pool({
+    user: 'hdelzyqgvzdsqp',
+    password: 'dc08a1f885ccabe215edfcb5d258afd186dd6c9d1e1b34136db96385792558d8', 
+    host: 'ec2-52-6-143-153.compute-1.amazonaws.com',
+    database: 'de4hg6rpacgnpp',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+});
+
+const sqlCreate = `
+    CREATE TABLE IF NOT EXISTS listacompras
+    (
+        ID serial primary key,
+        nome varchar(50) not null,
+        quantidade int not null default 0,
+        comprado boolean not null default false
+    )
+`;
+
+// pool.query(sqlCreate, function(error, result) {
+//     if(error)
+//         throw error
+
+//     console.log('Tabela criada com sucesso!');
+// });
+
+module.exports = {
+
+    async create(nome, quantidade) { 
+        const sql = `INSERT INTO listacompras (nome, quantidade)
+                        VALUES ($1, $2)`;
+
+        const result = await pool.query(sql, [nome, quantidade]);
+        return result.rowCount;
+    },
+
+    async read() {
+        const sql = 'SELECT * FROM listacompras'
+        const result = await pool.query(sql);
+        return result.rows;
+    }
+}
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const database = require('./database');
 
 const server = express();
 
-server.get('/', function(request, response) {
-    response.send('Lista de Compras, editado.');
+server.use(express.json());
+
+server.get('/', async function(request, response) {
+    const dados = await database.read();
+    return response.json(dados);
+})
+
+server.post('/', async function(request, response) { 
+
+    const nome = request.body.nome; // JSON
+    const quantidade = request.body.quantidade;
+
+    const result = await database.create(nome, quantidade);
+
+    return response.status(204).send();
 })
 
 server.listen(process.env.PORT || 3000);
