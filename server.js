ee04f657e856e65003c07ba4b0a152a9fb71f0fa
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-05-06 11:11:28 -0300

    Texto do GET editado.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', function(request, response) {
-    response.send('Lista de Compras');
+    response.send('Lista de Compras, editado.');
 })
 
 server.listen(process.env.PORT || 3000);
